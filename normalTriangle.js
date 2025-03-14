0592ec9922a00ee41be485c144fffdb73eb0b1db
Phiy-O <andikapiyo12@gmail.com>
2025-03-15 01:32:14 +0700

    normalTriangle.js

--- normalTriangle.js
+++ normalTriangle.js
@@ -1,3 +1,16 @@
+// normal Triangle
 for (let i = 1; i < 5; i++) {
     console.log("*".repeat(i))
+}
+
+// equilateral triangle
+for (let x = 1; x <= 4; x++) {
+    let y = 5;
+    console.log(" ".repeat(y-x) + "*".repeat(x) + "*".repeat(x-1))
+}
+
+// left side 
+for (let i = 1; i < 5; i++) {
+    let j = 4;
+    console.log(" ".repeat(j-i) + "*".repeat(i))
 }
