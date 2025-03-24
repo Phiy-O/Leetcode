43696ae756421baaa359083bd9e21bf44073022a
Phiy-O <andikapiyo12@gmail.com>
2025-03-24 14:49:04 +0700

    Update downTriangle.js

--- Javascript/downTriangle.js
+++ Javascript/downTriangle.js
@@ -1,1 +1,15 @@
+// right triangle
+for (let y = 4; y >= 0; y--) {
+    console.log("*".repeat(y+1))
+}
 
+// equilateral triangle
+for (let y = 4; y >= 0; y--) {
+    console.log(" ".repeat(4-y) + "*".repeat(y+1) + "*".repeat(y))
+}
+
+// left upside down
+for (let y = 5; y >= 0; y--) {
+    let z = 5;
+    console.log(" ".repeat(z-y) + "*".repeat(y))
+}
