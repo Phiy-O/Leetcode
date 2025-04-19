99f040c98397e87832fd4dde23d5379f3d17d122
Phiy-O <andikapiyo12@gmail.com>
2025-04-20 06:17:51 +0700

    Create fibbonaci.js

---
+++ Javascript/fibbonaci.js
@@ -0,0 +1,23 @@
+// 5. Fibbonaci
+
+function fib(n) {
+    let output = [];
+
+    if (n <= 0) {
+        output = [];
+    } else if (n == 1) {
+        output = [0];
+    } else if (n == 2) {
+        output = [0, 1];
+    } else {
+        output = [0, 1]
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1])
+        }
+    }
+
+    return output[output.length - 1];
+    // return output; -> you can call it like this also.
+}
+
+console.log(fib(9))
