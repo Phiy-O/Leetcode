dadb2120f9f17516fafe7da37393638b4a4549d8
Phiy-O <andikapiyo12@gmail.com>
2025-08-02 07:58:56 +0700

    Create fizz_buzz.py

---
+++ Python/fizz_buzz.py
@@ -0,0 +1,16 @@
+# [ Easy ] Fizz Buzz
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for x in range(1, n + 1):
+            if x % 15 == 0:
+                result.append("FizzBuzz")
+            elif x % 3 == 0:
+                result.append("Fizz")
+            elif x % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(x))
+        return result
+            
+        return result
