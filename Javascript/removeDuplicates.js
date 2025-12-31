2ce3e8d971ca7536b240fb50eef21416a2f31b39
Phiy-O <andikapiyo12@gmail.com>
2025-12-31 15:07:02 +0700

    Create removeDuplicates.js

---
+++ Javascript/removeDuplicates.js
@@ -0,0 +1,16 @@
+// 26. Remove Duplicates from Sorted Array - Easy (code explained by NeetCode)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let left = 1
+
+    for (let right = 1;right < nums.length;right++) {
+        if (nums[right] != nums[right - 1]) {
+            nums[left] = nums[right]
+            left++
+        }
+    }
+    return left
+};
