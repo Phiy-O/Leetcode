bf78037321e68974a95bceb52eb957f16df1b32c
Phiy-O <andikapiyo12@gmail.com>
2026-02-17 08:00:49 +0700

    Create findTheIndex.js

---
+++ Javascript/findTheIndex.js
@@ -0,0 +1,18 @@
+// 28. Find the Index of the First Occurrence in a String - EASY
+var merge = function(nums1, m, nums2, n) {
+    let arrLength = m + n;
+    let i = 0;
+    let j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (i >= m) {
+            nums1[i] = nums2[j]
+            j++
+        }
+        i++
+    }
+
+    const sortedArray = nums1.sort(function(a, b) {
+        return a - b
+    })
+};
